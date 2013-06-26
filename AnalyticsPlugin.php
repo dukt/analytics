0be0b7c3076b58584aacfb9aeacd2a012816c43c
Benjamin David <ben@dukt.net>
2013-06-26 07:43:47 +0200

    0.9.25

--- AnalyticsPlugin.php
+++ AnalyticsPlugin.php
@@ -30,7 +30,7 @@ class AnalyticsPlugin extends BasePlugin
      */
     function getVersion()
     {
-        return '0.9.24';
+        return '0.9.25';
     }
 
     // --------------------------------------------------------------------
