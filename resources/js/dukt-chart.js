f301fa95e4b5cf2fec78f423e035724a1022d9cc
Benjamin David <ben@dukt.net>
2013-06-26 07:04:03 +0200

    Improved chart loading

--- resources/js/dukt-chart.js
+++ resources/js/dukt-chart.js
@@ -2,13 +2,12 @@ $(document).ready(function() {
     charts = $('.dukt-chart');
 
     charts.each(function(k,v) {
-        var html = $(v).html();
-        console.log('#HTML', html);
+        var html = $('.data', v).html();
+
         var json = $.parseJSON(html);
-        console.log('#JSON', json);
 
         Craft.postActionRequest('analytics/charts/parse', json, function(response) {
-            console.log("XXXX", json);
+
             response = $.parseJSON(response);
             $(v).dchart(response, json);
             // $(v).html(response);
