31175ecb04061a02a199b36e68dbb1b0c199b5ba
Benjamin David <ben@dukt.net>
2013-06-28 03:27:30 +0200

    Added $referer private variable

--- controllers/Analytics_PluginController.php
+++ controllers/Analytics_PluginController.php
@@ -17,6 +17,7 @@ class Analytics_PluginController extends BaseController
     // --------------------------------------------------------------------
 
     private $pluginHandle = 'analytics';
+    private $referer;
 
     // --------------------------------------------------------------------
 
