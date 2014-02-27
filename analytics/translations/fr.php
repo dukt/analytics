5a779a2907cbcdb554d01d8ff50a31ed06c3b9d3
Benjamin David <ben@dukt.net>
2014-02-27 14:27:34 +0100

    French translation

---
+++ analytics/translations/fr.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Craft Analytics by Dukt
+ *
+ * @package   Craft Analytics
+ * @author    Benjamin David
+ * @copyright Copyright (c) 2013, Dukt
+ * @license   http://dukt.net/craft/analytics/docs#license
+ * @link      http://dukt.net/craft/analytics/
+ */
+
+return array(
+
+    'All' => "Toutes",
+    'Analytics Acquisition' => "Analytics Acquisition",
+    'Analytics Counts' => "Analytics Compteurs",
+    'Analytics Geo' => "Analytics Geo",
+    'Analytics Mobile' => "Analytics Mobile",
+    'Analytics Pages' => "Analytics Pages",
+    'Analytics plugin requires OAuth plugin for Craft.' => "Le plugin Analytics requiert le plugin OAuth pour Craft.",
+    'Analytics Settings' => "Paramètres Analytics",
+    'Analytics Technology' => "Analytics Technologie",
+    'Analytics Visits' => "Analytics Visites",
+    'AVG Time On Page' => "Temps Moy. sur la page",
+    'Bounce Rate' => "Taux de rebonds",
+    'Browser' => "Navigateur",
+    'Check Google API key & secret in OAuth settings' => "Vérifiez la clé et le code secret dans les paramètres OAuth",
+    'City' => "Ville",
+    'Columns' => "Colonnes",
+    'Connect to Google Analytics' => "Connexion à Analytics",
+    'Connect' => "Connexion",
+    'Country' => "Pays",
+    'Disconnect' => "Déconnexio",
+    'Download OAuth plugin' => "Télécharger le plugin OAuth",
+    'Exit Rate' => "Taux de sortie",
+    'Exit' => "Sortie",
+    'Getting errors trying to connect' => "Vous avez des problèmes pour vous connecter",
+    'Google Analytics Account' => "Compte Google Analytics",
+    'Google OAuth settings' => "Paramètres OAuth de Google",
+    'Google provider not configured' => "Le service Google n'est pas configuré",
+    'Interval in seconds between request to real-time API' => "Interval en secondes entre les requêtes vers l'API temps réel",
+    'Is Mobile' => "Mobile",
+    'Keywords' => "Mots clés",
+    'Landing' => "Page de destination",
+    'Language' => "Langue",
+    'Less' => "Réduire",
+    'Manage plugins' => "Gérer les plugins",
+    'Mobile Devices' => "Appareils mobiles",
+    'Month' => "Mois",
+    'More Data' => "Plus de données",
+    'Name' => "Nom",
+    'Networks' => "Réseaux",
+    'OAuth is installed but is disabled. Please enable it.' => "OAuth est installé mais désactivé. Veuillez l'activer",
+    'OAuth is ready' => "OAuth est prêt",
+    'OAuth Not Installed' => "OAuth n'est pas installé",
+    'OAuth plugin is present but not installed. Please install & enable it.' => "Le plugin OAuth est présent mais n'est pas installé. Veuillez l'installer",
+    'OS' => "OS",
+    'Page Views / Visit' => "Pages Vues / Visite",
+    'Page Views' => "Pages Vues",
+    'Please fill API client id & secret for Analytics provider.' => "Veuillez remplir l'ID et le secret du service Analytics.",
+    'Plugin Settings' => "Paramètres du plugin",
+    'Real-Time Refresh Interval' => "Interval de rafraîchissement du temps réel",
+    'Reports' => "Rapports",
+    'Resolutions' => "Résolutions",
+    'Save' => "Enregistrer",
+    'Select an Analytics web profile to associate with your Craft website' => "Sélectionnez un profil web Analytics à associer à votre site Craft",
+    'Sources' => "Sources",
+    'Type' => "Type",
+    'UID' => "UID",
+    'Visits' => "Visites",
+    'Web Profile' => "Profil Web",
+    'Week' => "Semaine",
+    'Year' => "Année",
+    'You are authenticated to Google Analytics with the following account' => "Vous êtes connecté à Google Analytics avec le compte suivant",
+    'You need to connect Craft to a Google account in order to get started.' => "Pour commencer, connectez Craft à Google Analytics",
+);
