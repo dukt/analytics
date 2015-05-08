2f8b1f9293e9d72a974791417c5ec4be2f3d52eb
Benjamin David <ben@dukt.net>
2015-05-08 03:59:01 +0200

    Added build script

---
+++ Utils/zip.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export PLUGIN_NAME="duktci"
+export SOURCE_FOLDER="Source/"
+export BUILD_FOLDER="Build/"
+export BUILD_PLUGIN_FOLDER="${BUILD_FOLDER}/${PLUGIN_NAME}/"
+export PLUGIN_NAME_WITH_VERSION="${PLUGIN_NAME}-3.0.${BUILD_NUMBER}"
+
+# Copy Build
+
+rm -rf $BUILD_FOLDER
+mkdir $BUILD_FOLDER
+cp -R $SOURCE_FOLDER $BUILD_PLUGIN_FOLDER
+
+# Clean up Build/
+
+find ./$BUILD_PLUGIN_FOLDER -name ".git" -exec rm -rf {} \;
+rm ./$BUILD_PLUGIN_FOLDER/composer.json
+rm ./$BUILD_PLUGIN_FOLDER/composer.lock
+
+# zip
+
+rm -rf Zip/
+mkdir Zip/
+cp -R $BUILD_PLUGIN_FOLDER Zip/$PLUGIN_NAME_WITH_VERSION
+
+cd Zip/
+zip -r "${PLUGIN_NAME_WITH_VERSION}.zip" $PLUGIN_NAME_WITH_VERSION
+cd ../
+
+ls -la
+
+# mv "${FOLDERNAME}.zip" "../zip/${FOLDERNAME}.zip"
