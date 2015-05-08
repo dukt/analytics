06b42bb0188a129b7e162ba9ebd21947664ffb98
Benjamin David <ben@dukt.net>
2015-05-08 10:54:39 +0200

    Improved zip.sh

--- Utils/zip.sh
+++ Utils/zip.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export PLUGIN_NAME="analytics"
 export SOURCE_FOLDER="Source/"
 export BUILD_FOLDER="Build/"
+export ZIP_FOLDER="Zip/"
 export BUILD_PLUGIN_FOLDER="${BUILD_FOLDER}/${PLUGIN_NAME}/"
 export PLUGIN_NAME_WITH_VERSION="${PLUGIN_NAME}-${PLUGIN_VERSION}.${BUILD_NUMBER}"
 
@@ -20,15 +20,13 @@ rm ./$BUILD_PLUGIN_FOLDER/composer.lock
 
 # zip
 
-rm -rf Zip/
-mkdir Zip/
-cp -R $BUILD_PLUGIN_FOLDER Zip/$PLUGIN_NAME_WITH_VERSION
+rm -rf $ZIP_FOLDER
+mkdir $ZIP_FOLDER
 
-cd Zip/
+# Copy build to zip folder and give its final name
+cp -R $BUILD_PLUGIN_FOLDER $ZIP_FOLDER$PLUGIN_NAME_WITH_VERSION
+
+cd $ZIP_FOLDER
 zip -r "${PLUGIN_NAME_WITH_VERSION}.zip" $PLUGIN_NAME_WITH_VERSION
 rm -rf $PLUGIN_NAME_WITH_VERSION
 cd ../
-
-ls -la
-
-# mv "${FOLDERNAME}.zip" "../zip/${FOLDERNAME}.zip"
