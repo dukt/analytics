6e0eb1697751e3f2725fc0655ef127e39a73aecf
Benjamin David <ben@dukt.net>
2015-05-08 13:12:27 +0200

    Removed git tag creation

--- Utils/setVersion.sh
+++ Utils/setVersion.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Create Git Tag
-pwd
-ls -la
-
-git tag $PLUGIN_VERSION.$BUILD_NUMBER
-git push origin $PLUGIN_VERSION.$BUILD_NUMBER
-
 # Create Info.php with plugin version constant
 
 cat > Source/$PLUGIN_NAME/Info.php << EOF
