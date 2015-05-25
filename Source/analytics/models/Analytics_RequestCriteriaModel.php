5d61d7f476eb9e601ab5fe10f9d649a9af8a4ce0
Benjamin David <ben@dukt.net>
2015-05-25 13:18:45 +0200

    Added Analytics_RequestCriteriaModel

---
+++ Source/analytics/models/Analytics_RequestCriteriaModel.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @link      https://dukt.net/craft/analytics/
+ * @copyright Copyright (c) 2015, Dukt
+ * @license   https://dukt.net/craft/analytics/docs/license
+ */
+
+namespace Craft;
+
+class Analytics_RequestCriteriaModel extends BaseModel
+{
+    protected function defineAttributes()
+    {
+        return array(
+            'ids' => AttributeType::String,
+            'startDate' => AttributeType::String,
+            'endDate' => AttributeType::String,
+            'metrics' => AttributeType::String,
+            'options' => array(AttributeType::Mixed, 'default' => array()),
+            'format' => AttributeType::String,
+            'realtime' => array(AttributeType::Bool, 'default' => false),
+        );
+    }
+
+    public function send()
+    {
+        $response = array(
+            'success' => false,
+            'error' => false
+        );
+
+        try
+        {
+            $response['data'] = craft()->analytics->sendRequest($this);
+
+            if(!isset($options['format']) || (isset($options['format']) && $options['format'] != 'gaData'))
+            {
+
+                $response['cols'] = $response['data']['cols'];
+                $response['rows'] = $response['data']['rows'];
+            }
+
+            $response['success'] = true;
+        }
+        catch(\Exception $e)
+        {
+            $response['error'] = true;
+            $response['errorMessage'] = $e->getMessage();
+        }
+
+        return $response;
+    }
+}
