c180f6b97fb96482ab05753ce4b9f3679e329405
Benjamin David <ben@dukt.net>
2015-07-29 23:27:12 +0200

    Display message showing version and build

--- Utils/build.sh
+++ Utils/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+export PLUGIN_VERSION_VAR="${CI_BRANCH}_VERSION"
+export PLUGIN_VERSION=${!PLUGIN_VERSION_VAR}
+export PLUGIN_VERSION_BUILD="${PLUGIN_VERSION}.${CI_BUILD_NUMBER}"
+
 export SOURCE_FOLDER="Source/"
+
+echo "Preparing build ${PLUGIN_VERSION_BUILD}"
 
 # Build Source
 
