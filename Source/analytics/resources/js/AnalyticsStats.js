f1bc3104f68c9109cade77285c89a3a80fa42e2d
Benjamin David <ben@dukt.net>
2015-07-29 23:06:44 +0200

    Improved CSS & JS

--- Source/analytics/resources/js/AnalyticsStats.js
+++ Source/analytics/resources/js/AnalyticsStats.js
@@ -218,23 +218,6 @@ Analytics.Stats = Garnish.Base.extend({
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /**
  * AnalyticsUtils
  */
@@ -248,35 +231,40 @@ Analytics.Utils = {
             data.addColumn(column);
         });
 
+        console.log('response', response);
 
         $.each(response.rows, function(kRow, row) {
             $.each(row, function(kCell, cell) {
-                switch(response.cols[kCell]['id'])
+
+                switch(response.cols[kCell]['type'])
                 {
-                    case 'ga:date':
+                    case 'date':
 
                         $dateString = cell.v;
 
-                        $year = eval($dateString.substr(0, 4));
-                        $month = eval($dateString.substr(4, 2)) - 1;
-                        $day = eval($dateString.substr(6, 2));
+                        if($dateString.length == 8)
+                        {
+                            // 20150101
 
-                        $date = new Date($year, $month, $day);
+                            $year = eval($dateString.substr(0, 4));
+                            $month = eval($dateString.substr(4, 2)) - 1;
+                            $day = eval($dateString.substr(6, 2));
 
-                        row[kCell] = $date;
+                            $date = new Date($year, $month, $day);
 
-                        break;
+                            row[kCell] = $date;
+                        }
+                        else if($dateString.length == 6)
+                        {
+                            // 201501
 
-                    case 'ga:yearMonth':
+                            $year = eval($dateString.substr(0, 4));
+                            $month = eval($dateString.substr(4, 2)) - 1;
 
-                        $dateString = cell.v;
+                            $date = new Date($year, $month, '01');
 
-                        $year = eval($dateString.substr(0, 4));
-                        $month = eval($dateString.substr(4, 2)) - 1;
-
-                        $date = new Date($year, $month, '01');
-
-                        row[kCell] = $date;
+                            row[kCell] = $date;
+                        }
 
                         break;
                 }
@@ -286,106 +274,7 @@ Analytics.Utils = {
         });
 
         return data;
-    },
-
-    parseColumn: function(apiColumn)
-    {
-        $type = 'string';
-
-        if(apiColumn.dataType == 'INTEGER'
-            || apiColumn.dataType == 'FLOAT'
-            || apiColumn.dataType == 'PERCENT'
-            || apiColumn.dataType == 'CURRENCY'
-            || apiColumn.dataType == 'TIME')
-        {
-            $type = 'number';
-        }
-
-        if(apiColumn.name == 'ga:date')
-        {
-            $type = 'date';
-            apiColumn.dataType = 'DATE';
-        }
-        if(apiColumn.name == 'ga:latitude')
-        {
-            $type = 'number';
-        }
-        if(apiColumn.name == 'ga:longitude')
-        {
-            $type = 'number';
-        }
-
-        if(apiColumn.name == 'ga:yearMonth')
-        {
-            $type = 'date';
-            apiColumn.dataType = 'DATE';
-        }
-
-        var column = {
-            'type': $type,
-            'dataType': apiColumn.dataType,
-            'name': apiColumn.name,
-            'label': apiColumn.label
-        };
-
-        return column;
-    },
-
-    parseRows: function(apiColumns, apiRows)
-    {
-        var rows = [];
-
-        if (typeof(apiRows) == 'undefined')
-        {
-            return rows;
-        };
-
-        $.each(apiRows, function(k, row) {
-
-            var cells = [];
-
-            $.each(apiColumns, function(k2, column) {
-
-                column = AnalyticsUtils.parseColumn(column);
-
-                var cell = apiRows[k][k2];
-
-                if(column.dataType == 'DATE')
-                {
-                    if(typeof(cell) == 'object')
-                    {
-                        $date = cell.v;
-                    }
-                    else
-                    {
-                        $date = cell;
-                    }
-
-                    $year = eval($date.substr(0, 4));
-                    $month = eval($date.substr(5, 2)) - 1;
-                    $day = eval($date.substr(8, 2));
-
-                    newDate = new Date($year, $month, $day);
-
-                    if(typeof($date) == 'object')
-                    {
-                        cell.v = newDate;
-                        cell.f = 'x';
-                    }
-                    else
-                    {
-                        cell = newDate;
-                    }
-                }
-
-                cells[k2] = cell;
-            });
-
-            rows[k] = cells;
-        });
-
-        return rows;
-    },
+    }
 };
 
 
