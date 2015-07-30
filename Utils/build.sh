ccdf79570d70e191fd081f2fb87b57b85b86c5b0
Benjamin David <ben@dukt.net>
2015-07-30 02:47:57 +0200

    Removed CI variables, improved composer install

--- Utils/build.sh
+++ Utils/build.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-echo $CI
-echo $CI_BUILD_NUMBER
-echo $CI_BUILD_URL
-echo $CI_PULL_REQUEST
-echo $CI_BRANCH
-echo $CI_COMMIT_ID
-echo $CI_COMMITTER_NAME
-echo $CI_COMMITTER_EMAIL
-echo $CI_COMMITTER_USERNAME
-echo $CI_MESSAGE
-echo $CI_NAME
-
 export SOURCE_FOLDER="Source/"
 
 # Build Source
 
 cd $SOURCE_FOLDER
 
-composer update
+composer install --prefer-source --no-interaction
 
 cd ../
