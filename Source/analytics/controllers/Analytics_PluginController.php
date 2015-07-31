93d3aaf6b5d9c45bf5888b9a4e1ecaf0025b6090
Benjamin David <ben@dukt.net>
2015-07-31 14:57:05 +0200

    Test controller

--- Source/analytics/controllers/Analytics_PluginController.php
+++ Source/analytics/controllers/Analytics_PluginController.php
@@ -7,6 +7,8 @@
 
 namespace Craft;
 
+use Guzzle\Http\Client;
+
 class Analytics_PluginController extends BaseController
 {
     // Properties
@@ -24,6 +26,29 @@ class Analytics_PluginController extends BaseController
 
     // Public Methods
     // =========================================================================
+
+    public function actionTest()
+    {
+        $pluginHandle = 'analytics';
+
+        $url = 'http://dukt.dk/api/updates?plugin='.$pluginHandle;
+
+        $client = new Client();
+
+        $response = $client->get($url)->send();
+
+        $updates = $response->json();
+
+        echo '<h1>Updates</h1>';
+
+        foreach($updates as $update)
+        {
+            echo $update['tag'];
+            echo '<hr />';
+        }
+
+        die();
+    }
 
     /**
      * Constructor
