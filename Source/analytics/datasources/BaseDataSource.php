342847dfd7e7a4642871ea54b6857ddad1deb03d
Benjamin David <ben@dukt.net>
2015-08-14 21:37:16 +0200

    Fixed BaseDataSource

--- Source/analytics/datasources/BaseDataSource.php
+++ Source/analytics/datasources/BaseDataSource.php
@@ -1,16 +1,9 @@
 <?php
 
-<<<<<<< HEAD
-class BaseDataSource {
-    public function getSettingsHtml()
-    {
-
-    }
-=======
 namespace Dukt\Analytics\DataSources;
 
 abstract class BaseDataSource
 {
-    
->>>>>>> feature/single-source
+
+
 }
