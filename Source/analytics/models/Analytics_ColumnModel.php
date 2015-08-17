baaab942a37c8a5007010b5e1366af69c41c4d01
Benjamin David <ben@dukt.net>
2015-08-17 02:30:30 +0200

    Added Analytics_ColumnModel

---
+++ Source/analytics/models/Analytics_ColumnModel.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @link      https://dukt.net/craft/analytics/
+ * @copyright Copyright (c) 2015, Dukt
+ * @license   https://dukt.net/craft/analytics/docs/license
+ */
+
+namespace Craft;
+
+class Analytics_ColumnModel extends BaseModel
+{
+    protected function defineAttributes()
+    {
+        return array(
+            'id' => AttributeType::String,
+            'type' => AttributeType::String,
+            'group' => AttributeType::String,
+            'status' => AttributeType::String,
+            'uiName' => AttributeType::String,
+            'description' => AttributeType::String,
+            'allowInSegments' => AttributeType::Bool,
+        );
+    }
+}
