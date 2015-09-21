c814d8c83a4d295f9cf9e912e4b33d8d9f094631
Benjamin David <ben@dukt.net>
2015-09-21 22:38:41 +0200

    Added migration that splits the Explorer widget into Realtime and Chart widgets

---
+++ Source/analytics/migrations/m150921_000001_explorer_widget_to_realtime_and_charts.php
@@ -0,0 +1,79 @@
+<?php
+namespace Craft;
+
+/**
+ * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_pluginHandle_migrationName
+ */
+class m150921_000001_explorer_widget_to_realtime_and_charts extends BaseMigration
+{
+    /**
+     * Any migration code in here is wrapped inside of a transaction.
+     *
+     * @return bool
+     */
+    public function safeUp()
+    {
+        $rows = craft()->db->createCommand()
+            ->select('*')
+            ->from('widgets')
+            ->where('type=:type', array(':type'=>'Analytics_Explorer'))
+            ->queryAll();
+
+        if($rows)
+        {
+            foreach($rows as $row)
+            {
+                $oldSettings = JsonHelper::decode($row['settings']);
+
+
+                // old to new
+
+                $newSettings = [];
+                $newSettings['colspan'] = $oldSettings['colspan'];
+
+
+                if(isset($oldSettings['chart']))
+                {
+                    $newSettings['chart'] = $oldSettings['chart'];
+                }
+
+                if(isset($oldSettings['period']))
+                {
+                    $newSettings['period'] = $oldSettings['period'];
+                }
+
+                $newSettings['options'] = [];
+
+                if(isset($oldSettings['dimension']))
+                {
+                    $newSettings['options']['dimension'] = $oldSettings['dimension'];
+                }
+
+                if(isset($oldSettings['metric']))
+                {
+                    $newSettings['options']['metric'] = $oldSettings['metric'];
+                }
+
+                switch($oldSettings['menu'])
+                {
+                    case 'realtimeVisitors':
+                        $type='Analytics_Realtime';
+                        break;
+
+                    default:
+                        $type='Analytics_Chart';
+                }
+
+
+                // update row
+
+                $newSettings = JsonHelper::encode($newSettings);
+
+                $updateCmd = craft()->db->createCommand()
+                    ->update('widgets', array('type' => $type, 'settings' => $newSettings), 'id=:id', array('id' => $row['id']));
+            }
+        }
+
+        return true;
+    }
+}
