dd13f5f6fc48d97758f711e412333007d9709764
Benjamin David <ben@dukt.net>
2016-08-28 21:52:20 +0200

    Improved styles for charts

--- resources/js/Analytics.js
+++ resources/js/Analytics.js
@@ -659,7 +659,9 @@ Analytics.reports.Pie = Analytics.reports.BaseChart.extend(
 		this.dataTable = Analytics.Utils.responseToDataTable(this.data.chart);
 		this.chartOptions = Analytics.ChartOptions.pie();
 		this.chart = new google.visualization.PieChart(this.$graph.get(0));
-		
+
+		this.chartOptions.height = this.$graph.height();
+
 		this.addChartReadyListener();
 	}
 });
