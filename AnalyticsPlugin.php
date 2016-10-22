97e539ea35b62659b2b7f5621bfa5e3a2ab1ed83
Benjamin David <ben@dukt.net>
2016-10-22 15:41:52 +0200

    Removed unused route

--- AnalyticsPlugin.php
+++ AnalyticsPlugin.php
@@ -141,7 +141,6 @@ class AnalyticsPlugin extends BasePlugin
 			'analytics/tests/columnGroups' => array('action' => "analytics/tests/columnGroups"),
 			'analytics/tests/formatting' => array('action' => "analytics/tests/formatting"),
 			'analytics/tests/reportWidgets' => array('action' => "analytics/tests/reportWidgets"),
-			'analytics/tests/accountExplorer' => array('action' => "analytics/tests/accountExplorer"),
 		);
 	}
 
