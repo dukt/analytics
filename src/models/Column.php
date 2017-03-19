538e92105c94a8e2c97c438269d3afa8a04d0607
Benjamin David <ben@dukt.net>
2017-03-19 14:05:27 +0100

    Docs

--- src/models/Column.php
+++ src/models/Column.php
@@ -9,20 +9,61 @@ namespace dukt\analytics\models;
 
 use craft\base\Model;
 
+/**
+ * Class Column
+ *
+ * @package dukt\analytics\models
+ */
 class Column extends Model
 {
     // Properties
     // =========================================================================
 
+    /**
+     * @var string Column ID
+     */
     public $id;
+
+    /**
+     * @var string Column Type
+     */
     public $type;
+
+    /**
+     * @var string Data Type
+     */
     public $dataType;
+
+    /**
+     * @var string Group
+     */
     public $group;
+
+    /**
+     * @var string Status
+     */
     public $status;
+
+    /**
+     * @var string UI Name
+     */
     public $uiName;
+
+    /**
+     * @var string Description
+     */
     public $description;
+
+    /**
+     * @var bool Allow in segments
+     */
     public $allowInSegments;
+
+    /**
+     * @var int Added in API version
+     */
     public $addedInApiVersion;
+
     // Public Methods
     // =========================================================================
 
