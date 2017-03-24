b92021f7f67327d8676238505c70720e9d991253
Benjamin David <ben@dukt.net>
2017-03-24 23:47:53 +0100

    Added `m170324_000002_remove_old_widgets` migration

---
+++ src/migrations/m170324_000002_remove_old_widgets.php
@@ -0,0 +1,36 @@
+<?php
+namespace dukt\analytics\migrations;
+
+use craft\db\Migration;
+
+/**
+ * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_pluginHandle_migrationName
+ */
+class m170324_000002_remove_old_widgets extends Migration
+{
+    /**
+     * Any migration code in here is wrapped inside of a transaction.
+     *
+     * @return bool
+     */
+    public function safeUp()
+    {
+        $this->delete('{{%widgets}}', ['type' => 'Analytics_Chart']);
+        $this->delete('{{%widgets}}', ['type' => 'Analytics_Stats']);
+        $this->delete('{{%widgets}}', ['type' => 'Analytics_Reports']);
+        $this->delete('{{%widgets}}', ['type' => 'dukt\analytics\widgets\Report', 'enabled' => 0]);
+        $this->delete('{{%widgets}}', ['type' => 'dukt\analytics\widgets\Realtime', 'enabled' => 0]);
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m170324_000002_remove_old_widget cannot be reverted.\n";
+
+        return false;
+    }
+}
