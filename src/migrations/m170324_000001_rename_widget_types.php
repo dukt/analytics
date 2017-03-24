eea5712d19f9915565ad5188ce5e05ccd4e6f94c
Benjamin David <ben@dukt.net>
2017-03-24 23:47:41 +0100

    Added `m170324_000001_rename_widget_types` migration

---
+++ src/migrations/m170324_000001_rename_widget_types.php
@@ -0,0 +1,33 @@
+<?php
+namespace dukt\analytics\migrations;
+
+use craft\db\Migration;
+
+/**
+ * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_pluginHandle_migrationName
+ */
+class m170324_000001_rename_widget_types extends Migration
+{
+    /**
+     * Any migration code in here is wrapped inside of a transaction.
+     *
+     * @return bool
+     */
+    public function safeUp()
+    {
+        $this->update('{{%widgets}}', ['type' => 'dukt\analytics\widgets\Report'], ['type' => 'Analytics_Report']);
+        $this->update('{{%widgets}}', ['type' => 'dukt\analytics\widgets\Realtime'], ['type' => 'Analytics_Realtime']);
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m170324_000001_rename_widget_types cannot be reverted.\n";
+
+        return false;
+    }
+}
