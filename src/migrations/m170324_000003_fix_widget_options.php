96c113714b9dde58ccdd3ea5ee61923c1e8737e9
Benjamin David <ben@dukt.net>
2017-03-25 00:31:55 +0100

    Added migration which fixes widget options

---
+++ src/migrations/m170324_000003_fix_widget_options.php
@@ -0,0 +1,57 @@
+<?php
+namespace dukt\analytics\migrations;
+
+use craft\db\Migration;
+use craft\db\Query;
+use craft\helpers\Json;
+
+/**
+ * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_pluginHandle_migrationName
+ */
+class m170324_000003_fix_widget_options extends Migration
+{
+    /**
+     * Any migration code in here is wrapped inside of a transaction.
+     *
+     * @return bool
+     */
+    public function safeUp()
+    {
+        $widgetResults = (new Query())
+            ->select('*')
+            ->from(['{{%widgets}}'])
+            ->where(['type' => 'dukt\analytics\widgets\Realtime'])
+            ->orWhere(['type' => 'dukt\analytics\widgets\Report'])
+            ->all();
+
+        if($widgetResults) {
+            foreach ($widgetResults as $result) {
+                $settings = Json::decode($result['settings']);
+                
+                if(isset($settings['options']['metric'])) {
+                    $settings['options'] = [
+                        $settings['chart'] => $settings['options']
+                    ];
+                }
+
+                // Update row
+
+                $settings = Json::encode($settings);
+
+                $this->update('{{%widgets}}', ['settings' => $settings], ['id' => $result['id']]);
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m170324_000003_fix_widget_options cannot be reverted.\n";
+
+        return false;
+    }
+}
