661d81ed3fe5c0963f81162bac1ee3b5c30cae07
Benjamin David <ben@dukt.net>
2022-04-07 23:52:45 +0200

    Improved settings

--- src/web/assets/analyticsvue/src/js/Settings.vue
+++ src/web/assets/analyticsvue/src/js/Settings.vue
@@ -1,104 +1,130 @@
 <template>
   <div>
-    <div
-      class="da-flex da-border da-rounded-md"
-    >
-      <div
-        class="da-flex-1 da-border-r"
-      >
-        <div
-          class="da-border-b da-font-medium da-text-gray-500 da-px-6 da-py-3"
-        >
-          Analytics Accounts
-        </div>
-        <div>
-          <ul>
-            <template v-for="(account, accountKey) in accounts">
-              <li
-                :key="accountKey"
-              >
-                <button
-                  class="da-px-6 da-py-3 da-block da-w-full da-text-left"
-                  :class="{
-                    'da-bg-gray-200': selectedAccount === account.id,
-                    'hover:da-bg-gray-100': selectedAccount !== account.id,
-                  }"
-                  @click.prevent="selectedAccount = account.id"
-                >
-                  {{ account.name }}
-                  <div
-                    class="da-text-gray-500"
-                  >
-                    {{ account.id }}
-                  </div>
-                </button>
-              </li>
-            </template>
-          </ul>
-        </div>
+    <template v-if="loading">
+      <div>
+        Loading…
       </div>
+    </template>
+    <template v-else>
       <div
-        class="da-flex-1 da-border-r"
+        class="da-flex da-border da-rounded-md"
       >
         <div
-          class="da-border-b da-font-medium da-text-gray-500 da-px-6 da-py-3"
+          class="da-flex-1 da-border-r"
         >
-          Properties &amp; Apps
-        </div>
-        <div>
-          <ul>
-            <template v-for="(property, propertyKey) in properties">
-              <li
-                :key="propertyKey"
-              >
-                <button
-                  class="da-px-6 da-py-3 da-block da-w-full da-text-left"
-                  :class="{
-                    'da-bg-gray-200': selectedProperty === property.id,
-                    'hover:da-bg-gray-100': selectedProperty !== property.id,
-                  }"
-                  @click.prevent="selectedProperty = property.id"
+          <div
+            class="da-border-b da-font-medium da-text-gray-500 da-px-6 da-py-3"
+          >
+            Analytics Accounts
+          </div>
+          <div>
+            <ul>
+              <template v-for="(account, accountKey) in accounts">
+                <li
+                  :key="accountKey"
                 >
-                  {{ property.name }}
-                  <div
-                    class="da-text-gray-500"
+                  <button
+                    class="da-px-6 da-py-3 da-block da-w-full da-text-left da-flex da-justify-between da-items-center"
+                    :class="{
+                      'da-bg-gray-100': selectedAccount === account.id,
+                      'hover:da-bg-gray-100/50': selectedAccount !== account.id,
+                    }"
+                    @click.prevent="selectedAccount = account.id"
                   >
-                    {{ property.id }}
-                  </div>
-                </button>
-              </li>
-            </template>
-          </ul>
-        </div>
-      </div>
-      <div
-        class="da-flex-1"
-      >
-        <div
-          class="da-border-b da-font-medium da-text-gray-500 da-px-6 da-py-3"
-        >
-          Views
-        </div>
-        <div>
-          <ul>
-            <template v-for="(view, viewKey) in views">
-              <li
-                :key="viewKey"
-                class="da-px-6 da-py-3"
-              >
-                {{ view.name }}
+                    <div>
+                      <div>
+                        {{ account.name }}
+                      </div>
+                      <div
+                        class="da-text-gray-500"
+                      >
+                        {{ account.id }}
+                      </div>
+                    </div>
 
-                <button
-                  class="da-bg-blue-600 da-text-white da-font-medium da-rounded-md da-px-2 da-py-1 da-ml-2"
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="da-h-4 da-w-4 da-text-gray-700"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M9 5l7 7-7 7"
+                      />
+                    </svg>
+                  </button>
+                </li>
+              </template>
+            </ul>
+          </div>
+        </div>
+        <div
+          class="da-flex-1 da-border-r"
+        >
+          <div
+            class="da-border-b da-font-medium da-text-gray-500 da-px-6 da-py-3"
+          >
+            Properties &amp; Apps
+          </div>
+          <div>
+            <ul>
+              <template v-for="(property, propertyKey) in properties">
+                <li
+                  :key="propertyKey"
                 >
-                  Select
-                </button>
-              </li>
-            </template>
-          </ul>
+                  <button
+                    class="da-px-6 da-py-3 da-block da-w-full da-text-left"
+                    :class="{
+                      'da-bg-gray-100': selectedProperty === property.id,
+                      'hover:da-bg-gray-100/50': selectedProperty !== property.id,
+                    }"
+                    @click.prevent="selectedProperty = property.id"
+                  >
+                    {{ property.name }}
+                    <div
+                      class="da-text-gray-500"
+                    >
+                      {{ property.id }}
+                    </div>
+                  </button>
+                </li>
+              </template>
+            </ul>
+          </div>
+        </div>
+        <div
+          class="da-flex-1"
+        >
+          <div
+            class="da-border-b da-font-medium da-text-gray-500 da-px-6 da-py-3"
+          >
+            Views
+          </div>
+          <div>
+            <ul>
+              <template v-for="(view, viewKey) in views">
+                <li
+                  :key="viewKey"
+                  class="da-px-6 da-py-3"
+                >
+                  {{ view.name }}
+
+                  <button
+                    class="da-bg-blue-600 da-text-white da-font-medium da-rounded-md da-px-2 da-py-1 da-ml-2"
+                  >
+                    Select
+                  </button>
+                </li>
+              </template>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </template>
   </div>
 </template>
 
@@ -107,11 +133,12 @@ import settingsApi from './api/settings'
 
 export default {
   data() {
-      return {
-        accountExplorerData: null,
-        selectedAccount: null,
-        selectedProperty: null,
-      }
+    return {
+      loading: false,
+      accountExplorerData: null,
+      selectedAccount: null,
+      selectedProperty: null,
+    }
   },
 
   computed: {
@@ -147,8 +174,10 @@ export default {
   },
 
   mounted() {
+    this.loading = true
     settingsApi.getAccountExplorerData()
       .then(response => {
+        this.loading = false
         this.accountExplorerData = response.data;
       });
   }
