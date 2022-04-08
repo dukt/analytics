6bd4cdb59b508781989b89599a9e8d99e2873340
Benjamin David <ben@dukt.net>
2022-04-08 08:53:24 +0200

    Improved report widget settings

--- src/web/assets/analyticsvue/src/js/ReportWidgetSettings.vue
+++ src/web/assets/analyticsvue/src/js/ReportWidgetSettings.vue
@@ -1,29 +1,132 @@
 <template>
   <div class="da-bg-gray-100 da-border da-rounded-md da-p-6">
-    <div>
-      <strong>View</strong>
+    <div class="field">
+      <div class="heading">
+        <label>View</label>
+      </div>
+      <div class="input">
+        <div class="select">
+          <select>
+            <template v-for="(option, optionKey) in viewOptions">
+              <option :key="optionKey">
+                {{ option.label }}
+              </option>
+            </template>
+          </select>
+        </div>
+      </div>
     </div>
-    <div>
-      <strong>Chart</strong>
 
-      <ul>
-        <li>Area</li>
-        <li>Count</li>
-        <li>Pie</li>
-        <li>Table</li>
-        <li>Geo</li>
-      </ul>
+    <div class="field">
+      <div class="heading">
+        <label>Chart Type</label>
+      </div>
+      <div class="input">
+        <div>
+          <div class="da-inline-block">
+            <ul class="da-flex da-border da-bg-white da-rounded-md">
+              <template v-for="(option, optionKey) in chartTypeOptions">
+                <li :key="optionKey">
+                  <div
+                    class="da-px-4 da-py-3 hover:da-bg-gray-100/50 da-cursor-pointer"
+                    :class="{
+                      'da-border-r': optionKey !== chartTypeOptions.length - 1,
+                    }"
+                  >
+                    {{ option.label }}
+                  </div>
+                </li>
+              </template>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-    <div>
-      <strong>Period</strong>
-      <ul>
-        <li>Week</li>
-        <li>Month</li>
-        <li>Year</li>
-      </ul>
+
+
+    <div class="field">
+      <div class="heading">
+        <label>Period</label>
+      </div>
+      <div class="input">
+        <div class="select">
+          <select>
+            <template v-for="(option, optionKey) in periodOptions">
+              <option :key="optionKey">
+                {{ option.label }}
+              </option>
+            </template>
+          </select>
+        </div>
+      </div>
     </div>
-    <div>
-      <strong>Metric</strong>
+
+    <div class="field">
+      <div class="heading">
+        <label>Metric</label>
+      </div>
+      <div class="input">
+        <div class="select">
+          <select>
+            <template v-for="(option, optionKey) in []">
+              <option :key="optionKey">
+                {{ option.label }}
+              </option>
+            </template>
+          </select>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      chartTypeOptions: [
+        {
+          label: 'Area',
+          value: 'area',
+        },
+        {
+          label: 'Count',
+          value: 'count',
+        },
+        {
+          label: 'Pie',
+          value: 'pie',
+        },
+        {
+          label: 'Table',
+          value: 'table',
+        },
+        {
+          label: 'Geo',
+          value: 'geo',
+        },
+      ],
+      viewOptions: [
+        {
+          label: 'Dukt.net',
+          value: 'dukt.Net'
+        }
+      ],
+      periodOptions: [
+        {
+          label: 'Week',
+          value: 'week'
+        },
+        {
+          label: 'Month',
+          value: 'month'
+        },
+        {
+          label: 'Year',
+          value: 'year'
+        }
+      ]
+    }
+  }
+}
+</script>
