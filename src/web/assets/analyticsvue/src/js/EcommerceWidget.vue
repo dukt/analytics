449edb0a8b4a12fc23433591ce268af53c213fac
Benjamin David <ben@dukt.net>
2022-04-22 10:28:28 +0200

    Added margin

--- src/web/assets/analyticsvue/src/js/EcommerceWidget.vue
+++ src/web/assets/analyticsvue/src/js/EcommerceWidget.vue
@@ -15,6 +15,7 @@
           chart-type="area"
           :chart-data="chartData"
           :chart-options="chartOptions"
+          class="da-mt-4"
         />
       </template>
       <div class="tiles">
