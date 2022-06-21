e6a81402bddc589f6187afe7883caf48bc316428
Benjamin David <ben@dukt.net>
2022-06-21 23:37:32 +0200

    Improved settings tests

--- tests/settings.spec.js
+++ tests/settings.spec.js
@@ -5,3 +5,21 @@ test('Shoud show the Settings page', async ({ page, context, baseURL }) => {
   const title = page.locator('h1');
   await expect(title).toHaveText('Analytics');
 });
+
+test('Shoud show the OAuth Settings page', async ({ page, context, baseURL }) => {
+  await page.goto(baseURL + '/analytics/settings/oauth');
+  const title = page.locator('h1');
+  await expect(title).toHaveText('OAuth Settings');
+});
+
+test('Shoud show the Views Settings page', async ({ page, context, baseURL }) => {
+  await page.goto(baseURL + '/analytics/settings/views');
+  const title = page.locator('h1');
+  await expect(title).toHaveText('Analytics');
+});
+
+test('Shoud show the Sites Settings page', async ({ page, context, baseURL }) => {
+  await page.goto(baseURL + '/analytics/settings/sites');
+  const title = page.locator('h1');
+  await expect(title).toHaveText('Analytics');
+});
