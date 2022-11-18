7423f857c764af24310de0545a37f9a0408f364a
Benjamin David <ben@dukt.net>
2022-11-18 08:32:39 +0100

    Fixed new widget menu selector

--- tests/widgets.spec.js
+++ tests/widgets.spec.js
@@ -6,13 +6,10 @@ test('Shoud show Analytics widgets in the Dashboard’s new widget menu', async 
   await expect(title).toHaveText('Dashboard');
 
   const handle = await page.$('#newwidgetmenubtn');
-
-  let menuId = (await handle.getAttribute('aria-controls')).replace('.', '\\.')
-
   await handle.click()
-  await page.waitForSelector(`#${menuId}`)
+  await page.waitForSelector('#new-widget-menu ul')
 
-  expect((await page.locator('ul#' + menuId + ' li a[data-type="dukt\\\\analytics\\\\widgets\\\\Realtime"]:has-text("Active users")').count() > 0)).toBeTruthy();
-  expect((await page.locator('ul#' + menuId + ' li a[data-type="dukt\\\\analytics\\\\widgets\\\\Report"]:has-text("Analytics Report")').count() > 0)).toBeTruthy();
-  expect((await page.locator('ul#' + menuId + ' li a[data-type="dukt\\\\analytics\\\\widgets\\\\Ecommerce"]:has-text("E-commerce")').count() > 0)).toBeTruthy();
+  expect((await page.locator('#new-widget-menu ul li a[data-type="dukt\\\\analytics\\\\widgets\\\\Realtime"]:has-text("Active users")').count() > 0)).toBeTruthy();
+  expect((await page.locator('#new-widget-menu ul li a[data-type="dukt\\\\analytics\\\\widgets\\\\Report"]:has-text("Analytics Report")').count() > 0)).toBeTruthy();
+  expect((await page.locator('#new-widget-menu ul li a[data-type="dukt\\\\analytics\\\\widgets\\\\Ecommerce"]:has-text("E-commerce")').count() > 0)).toBeTruthy();
 });
