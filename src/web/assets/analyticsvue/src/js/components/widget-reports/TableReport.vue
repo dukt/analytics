7e723aea57b935cf5787cffc93e3dccbedd84c8d
Benjamin David <ben@pixelandtonic.com>
2023-06-19 22:49:20 +0200

    Fixed styles of the table report

--- src/web/assets/analyticsvue/src/js/components/widget-reports/TableReport.vue
+++ src/web/assets/analyticsvue/src/js/components/widget-reports/TableReport.vue
@@ -8,6 +8,7 @@
       <div>
         <template v-if="chartData">
           <analytics-chart
+            class="table-report"
             :chart-type="reportResponse.data.type"
             :chart-data="chartData"
             :chart-options="chartOptions"
@@ -54,3 +55,136 @@ export default {
   }
 }
 </script>
+
+<style>
+.table-report {
+  overflow: visible;
+  z-index: 1;
+  margin: 0 0 0 0;
+  position: relative;
+  min-height: 30px;
+  border-top: 1px dotted #e3e5e8;
+  min-height: 60px;
+
+  & > div {
+    position: inherit;
+  }
+
+  td strong {
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    width: 200px;
+    display: block;
+    overflow: hidden;
+    font-weight: normal;
+  }
+
+  .google-visualization-table {
+    width: 100%;
+    border: 0;
+  }
+  .google-visualization-table-table {
+    width: 100%;
+  }
+
+  thead {
+    tr {
+      background: none;
+
+      .google-visualization-table-th {
+        background: none !important;
+        padding-left: 0;
+        padding-right: 0;
+        border: 0;
+        border-bottom: 1px dotted #e3e5e8;
+        color: rgba(0, 0, 0, 0.298039);
+        font-size: 0.8em;
+        white-space: nowrap;
+
+        &:first-child {
+          text-align: left;
+        }
+
+        &:last-child {
+          text-align: right;
+        }
+
+        span {
+          display: none;
+        }
+      }
+    }
+  }
+
+  td.google-visualization-table-td {
+    max-width: 100px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    background: #fff;
+    border-left: 0;
+    border-right: 0;
+    border-top: 0;
+    border-bottom: 1px dotted #e3e5e8;
+
+    &:hover {
+      background: #fff;
+    }
+
+    &.google-visualization-table-td-number {
+      text-align: right;
+    }
+  }
+
+  .google-visualization-table-tr-sel td,
+  .google-visualization-table-tr-over td {
+    background-color: #fff;
+  }
+
+  .google-visualization-table-tr-sel.google-visualization-table-tr-odd,
+  .google-visualization-table-tr-over.google-visualization-table-tr-odd {
+    background-color: #fff;
+  }
+
+  .google-visualization-table-div-page {
+    text-align: center;
+    position: relative;
+    width: 100%;
+    margin-top: 7px;
+    background: none;
+
+    .google-visualization-table-page-numbers {
+      display: none;
+    }
+
+    .goog-custom-button {
+      background: #fff;
+      border: 1px solid #ddd;
+      padding: 4px 14px;
+      border-radius: 4px;
+
+      &[role="button"].goog-custom-button-disabled {
+        cursor: default;
+      }
+
+      &:not(:last-child) {
+        margin-right: 7px;
+      }
+
+      .goog-custom-button-outer-box {
+        border: 0;
+
+        .goog-custom-button-inner-box {
+          border: 0;
+
+          span {
+            font-size: 1.8em;
+            line-height: 1em;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
