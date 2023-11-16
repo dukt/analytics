c57d5d3b886f1aa931e9466db6365c9292ed6488
Benjamin David <ben@pixelandtonic.com>
2023-11-17 00:41:13 +0100

    Added views to sources migration

---
+++ src/migrations/m231115_165849_views_to_sources.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace dukt\analytics\migrations;
+
+use craft\db\Migration;
+use craft\db\Query;
+use craft\helpers\Json;
+
+/**
+ * m231115_165849_views_to_sources migration.
+ */
+class m231115_165849_views_to_sources extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp(): bool
+    {
+        // Sources table
+        $this->renameTable('{{%analytics_views}}', '{{%analytics_sources}}');
+        $this->addColumn('{{%analytics_sources}}', 'type', $this->enum('type', ['UA', 'GA4'])->notNull()->after('name'));
+        $this->renameColumn('{{%analytics_sources}}', 'gaViewCurrency', 'gaCurrency');
+        $this->alterColumn('{{%analytics_sources}}', 'gaViewId', $this->string()->null());
+        $this->alterColumn('{{%analytics_sources}}', 'gaViewName', $this->string()->null());
+        $this->alterColumn('{{%analytics_sources}}', 'gaCurrency', $this->string()->null());
+
+        // Site sources table
+        $this->renameTable('{{%analytics_site_views}}', '{{%analytics_site_sources}}');
+        $this->renameColumn('{{%analytics_site_sources}}', 'viewId', 'sourceId');
+
+        // Rename `viewId` to `sourceId` in widgets
+        $widgetRows = (new Query())
+            ->select(['id', 'settings'])
+            ->from(['{{%widgets}}'])
+            ->where(['type' => 'dukt\analytics\widgets\Report'])
+            ->all();
+
+        foreach ($widgetRows as $widgetRow) {
+            $widgetSettings = Json::decodeIfJson($widgetRow['settings']);
+
+            if (is_array($widgetSettings) && isset($widgetSettings['viewId'])) {
+                $widgetSettings['sourceId'] = $widgetSettings['viewId'];
+                unset($widgetSettings['viewId']);
+
+                $this->update('{{%widgets}}', [
+                    'settings' => Json::encode($widgetSettings),
+                ], ['id' => $widgetRow['id']]);
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown(): bool
+    {
+        echo "m231115_165849_views_to_sources cannot be reverted.\n";
+        return false;
+    }
+}
