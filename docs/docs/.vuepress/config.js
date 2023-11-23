c34c05f9d4673e4b89771c1158013abd50be9fb2
Benjamin David <ben@pixelandtonic.com>
2023-11-23 09:34:15 +0100

    Removed tracking page

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -52,7 +52,6 @@ module.exports = {
                  collapsable: false,
                  children: [
                      'requesting-analytics-api',
-                     'tracking',
                  ]
              }
          ],
